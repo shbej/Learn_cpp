a739e9977c39a1bc1169839674a3130e665f2272
shbej <sh.bejanyan@gmail.com>
2020-06-02 17:49:22 +0400

    Add first practice in C++

---
+++ first practice.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::cout<<"   Hi there!\n  Hola!\n Salut!\nԲարև";
+    int x = 10;
+    int y = 20;
+    int sum = x + y;
+    std::cout << "Sum of x and y is "<< sum;
+}

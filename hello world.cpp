22b94b45b473d3cd17d08f53a5ae01ff64313d1b
shbej <sh.bejanyan@gmail.com>
2020-06-06 12:15:23 +0400

    Update hello world.cpp

--- hello world.cpp
+++ hello world.cpp
@@ -2,5 +2,17 @@
 
 int main()
 {
-  std::cout<<"H\ne\nl\nl\no\n\nw\no\nr\nl\nd\n!";
+  std::cout<<
+  "H\n"
+  "e\n"
+  "l\n"
+  "l\n"
+  "o\n"
+  " \n"
+  "w\n"
+  "o\n"
+  "r\n"
+  "l\n"
+  "d\n"
+  "!\n";
 }

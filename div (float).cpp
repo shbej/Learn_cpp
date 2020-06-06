b44c71c0b954998f86c063d17ab879e797d7b49d
shbej <sh.bejanyan@gmail.com>
2020-06-07 01:20:53 +0400

    Add float operations

---
+++ div (float).cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    long double x = 0;
+    long double y = 0;
+    std::cin >> x >> y;
+    long double div = x / y;
+    std::cout << div;
+}
---
+++ sum (float).cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    long double x = 0;
+    long double y = 0;
+    long double z = 0;
+    long double t = 0;
+    std::cin >> x >> y >> z >> t;
+    std::cout << (x+y) - (z+t);
+}
